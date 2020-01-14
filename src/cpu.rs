d8e46af4b1e6e7d83ecc3e5325a031a3fbb6ceec
Ben Krocke <benkrocke@hotmail.com>
2020-01-14 19:12:57 +0100

    CPU REGISTERS
    Added the CPU registers and an initialization function.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,1 +1,23 @@
 mod opcode_tests;
+
+struct Registers {
+    a: u8, // Accumulator
+    x: u8, // Index Register
+    y: u8, // Index Register
+    s: u8, // Stack Pointer
+    p: u8, // Status Register
+    pc: u16, // Program Counter
+}
+
+impl Registers {
+    fn new() -> Registers {
+        Registers {
+            a: 0,
+            x: 0,
+            y: 0,
+            s: 0xfd,
+            pc: 0xc000,
+            p: 0, //TODO: Needs different value most likely
+        }
+    }
+}
